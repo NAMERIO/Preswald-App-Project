b99b37e792c3fc5ed5d19ff60daa9e042a204f92
Natan <150294496+NAMERIO@users.noreply.github.com>
2025-05-13 19:27:40 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,14 +5,16 @@ connect()
 
 df = get_df("sample_csv")
 
-sql = "SELECT * FROM sample_csv WHERE TYPE = 'TV Show'"
-filtered_df = query(sql, "sample_csv") 
+df.columns = df.columns.str.lower()
+
+sql = "SELECT * FROM sample_csv WHERE type = 'TV Show'"
+filtered_df = query(sql, "sample_csv")
 
 text("# Netflix Data Analysis")
 text("This app shows data about Netflix shows.")
 
 table(filtered_df, title="Filtered Data")
 
-fig = px.bar(filtered_df, x="TITLE", y="SHOW_ID", title="Top Netflix Shows (TV Shows Only)", labels={'SHOW_ID': 'Show ID', 'TITLE': 'Show Title'})
+fig = px.bar(filtered_df, x="title", y="show_id", title="Top Netflix Shows (TV Shows Only)", labels={'show_id': 'Show ID', 'title': 'Show Title'})
 
 plotly(fig)
